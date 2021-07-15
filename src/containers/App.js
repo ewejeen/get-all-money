2f81acc86c8f873369164462f1d7d89384cd3261
ewejeen <yoojinkim24@gmail.com>
2021-07-15 23:28:34 +0900

    스프링 연결 테스트

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import {Main, Bank, Interest} from '../pages'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Route exact path="/" component = {Main} />
-        <Route path="/bank" component={Bank} />
-        <Route path="/interest" component={Interest} />
-      </div>
-    );
-  }
+function App() {
+  const [message, setMessage] = useState('');
+
+  // 스프링 연결 테스트 (localhost:8080)
+  useEffect(() => {
+    fetch('/hello')
+      .then(response => response.text())
+      .then(message => {
+        setMessage(message);
+      });
+    },[])
+
+  return (
+    <div>
+      <p>{message}</p>
+      <Route exact path="/" component = {Main} />
+      <Route path="/bank" component={Bank} />
+      <Route path="/interest" component={Interest} />
+    </div>
+  );
 }
 
 export default App;
+// class App extends Component {
+//   render() {
+//     const [message, setMessage] = useState('');
+
+//     useEffect(() => {
+//       fetch('/hello')
+//         .then(response => response.text())
+//         .then(message => {
+//           setMessage(message);
+//         });
+//       },[])
+
+//     return (
+//       <div>
+//         <Route exact path="/" component = {Main} />
+//         <Route path="/bank" component={Bank} />
+//         <Route path="/interest" component={Interest} />
+//       </div>
+//     );
+//   }
+// }
